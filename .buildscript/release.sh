f1c35358e66708c10c6495e73c3533a5ad9e1d8f
tramchamploo <guohang@staff.weibo.com>
2017-05-03 16:00:42 +0800

    Add encrypted identification

---
+++ .buildscript/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [[ $GIT_USER_EMAIL ]]; then
+  git config --global user.email "$GIT_USER_EMAIL"
+fi
+
+if [[ $GIT_USER_NAME ]]; then
+  git config --global user.name "$GIT_USER_NAME"
+fi
+
+if [[ $RELEASE ]]; then
+  mvn -B release:prepare -DreleaseVersion=$RELEASE -DdevelopmentVersion=$NEXT -DskipTests
+  mvn -B release:perform --settings=".buildscript/settings.xml"
+  sed -i "s/<version>\(.*\)</<version>$RELEASE</" README.md
+fi
